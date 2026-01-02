1c312bdeb3b0b6516dad34a73da86653916aa74a
christian <christian>
2026-01-02 11:29:34 +0200

    Initial Commit

---
+++ snmpproxyclient/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  snmpproxyclient
+//
+//  Created by Christian Meutes on 02.01.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ snmpproxyclient/snmpproxyclientApp.swift
@@ -0,0 +1,17 @@
+//
+//  snmpproxyclientApp.swift
+//  snmpproxyclient
+//
+//  Created by Christian Meutes on 02.01.26.
+//
+
+import SwiftUI
+
+@main
+struct snmpproxyclientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
